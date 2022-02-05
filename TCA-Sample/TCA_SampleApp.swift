cd325611f40c3f3abe4c9560e401076a6e43d579
Shuhei Kuroda <snow.shader@gmail.com>
2022-02-06 02:48:22 +0900

    とりあえずPackageだけ追加

---
+++ TCA-Sample/TCA_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_SampleApp.swift
+//  TCA-Sample
+//
+//  Created by Shuhei Kuroda on 2022/02/06.
+//
+
+import SwiftUI
+
+@main
+struct TCA_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
